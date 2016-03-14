cbb839e038a3d6dd317cdca6f4d0bedb21f9e084
Ghanan Gowripalan <ghanan94@gmail.com>
2016-03-13 20:03:32 -0400

    generateABPCSV

---
+++ generateABPCSV.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include "ABPSimulator.h"
+
+int main(int argc, char *argv[]) {
+  printf("ABPSimulator\n");
+
+  unsigned int successPackets = 10000;
+	unsigned int H =  54 * 8; // Bits
+	unsigned int l = 1500 * 8; // Bits
+	unsigned int C = 5 * 1024 * 1024; // Bits per second (bps)
+  bool ackNak = false;
+
+  double TAL = 5; // Milliseconds
+  double DELTA = TAL * 2.5; // Milliseconds
+  double BER = 0.0;
+
+  ABPSimulator abpsimulator(ackNak, H, l, DELTA, C, TAL, BER);
+  double factor;
+  double throughput;
+  std::ofstream csvFile;
+  
+  csvFile.open("ABP.csv");
+
+  for (int i = 1; i <= 5; ++i) {
+    factor = 2.5 * i;
+
+    TAL = 5;
+    DELTA = TAL * factor;
+    BER = 0.0;
+    abpsimulator.timeoutTime = DELTA;
+    abpsimulator.propagationDelay = TAL;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << ",";
+
+    BER = 1e-5;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << ",";
+
+    BER = 1e-4;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << ",";
+
+    TAL = 250;
+    DELTA = TAL * factor;
+    BER = 0.0;
+    abpsimulator.timeoutTime = DELTA;
+    abpsimulator.propagationDelay = TAL;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << ",";
+
+    BER = 1e-5;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << ",";
+
+    BER = 1e-4;
+    abpsimulator.bitErrorRate = BER;
+    throughput = abpsimulator.simulate(successPackets);
+    csvFile << throughput << "\n";
+  }
+
+  csvFile.close();
+
+	exit(EXIT_SUCCESS);
+}
--- makefile
+++ makefile
@@ -4,21 +4,18 @@ OBJECT_DIR = obj
 
 .PHONY: default all clean
 
-default: lab2
+default: run
 all: default
 
 HEADERS = $(wildcard *.h)
-LAB2_OBJECTS = $(OBJECT_DIR)/lab2.o $(OBJECT_DIR)/ABPSimulator.o
 ABPSIMULATOR_DEBUG_OBJECTS = $(OBJECT_DIR)/ABPSimulator_debug.o $(OBJECT_DIR)/ABPSimulator.o
 GBNSIMULATOR_DEBUG_OBJECTS= $(OBJECT_DIR)/GBNSimulator_debug.o $(OBJECT_DIR)/GBNSimulator.o
+GENERATE_ABP_CSV_OBJECTS = $(OBJECT_DIR)/generateABPCSV.o $(OBJECT_DIR)/ABPSimulator.o
 
 $(OBJECT_DIR)/%.o: %.cpp $(HEADERS)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 .PRECIOUS: $(TARGET) $(OBJECTS)
-
-lab2: $(LAB2_OBJECTS)
-	$(CC) $(CFLAGS) $(LAB2_OBJECTS) -o $@
 
 ABPSimulator_debug: $(ABPSIMULATOR_DEBUG_OBJECTS)
 	$(CC) $(CFLAGS) $(ABPSIMULATOR_DEBUG_OBJECTS) -o $@
@@ -26,8 +23,14 @@ ABPSimulator_debug: $(ABPSIMULATOR_DEBUG_OBJECTS)
 GBNSimulator_debug: $(GBNSIMULATOR_DEBUG_OBJECTS)
 	$(CC) $(CFLAGS) $(GBNSIMULATOR_DEBUG_OBJECTS) -o $@
 
+generateABPCSV: $(GENERATE_ABP_CSV_OBJECTS)
+	$(CC) $(CFLAGS) $(GENERATE_ABP_CSV_OBJECTS) -o $@
+
+run: generateABPCSV
+
+
 clean:
 	-rm -f $(OBJECT_DIR)/*.o
-	-rm -f lab2
+	-rm -f generateABPCSV
 	-rm -f ABPSimulator_debug
 	-rm -f GBNSimulator_debug
